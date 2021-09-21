277d3d46d379b84e4ba18689f210c2f92d6668f3
Rinzler8806 <shane.rogers916@gmail.com>
2021-09-20 22:56:01 -0700

    res.sendFile(path)

--- myApp.js
+++ myApp.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res) {
-    res.send('Hello Express');
+    res.send('__dirname + /views/index.html');
 });
-
-
 
 
 
