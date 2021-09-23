306f528a6c39778b91481d513b58e8649d2e2158
Rinzler8806 <shane.rogers916@gmail.com>
2021-09-23 09:28:48 -0700

    changed env file to include MESSAGE_STYLE='uppercase';

--- myApp.js
+++ myApp.js
@@ -3,8 +3,10 @@ var app = express();
 
 app.use("/public", express.static(__dirname + '/public'));
 
+// process.env.MESSAGE_STYLE='uppercase';
+
 app.get("/JSON", function(req, res) {
-    if process.env.JSON=UPPERCASE({"message": "HELLO JSON"});
+    var response=({"message": "HELLO JSON"});
 });
 
 
