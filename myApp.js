b062ec971fa803c04690023abb70eb26397fe877
Rinzler8806 <shane.rogers916@gmail.com>
2021-09-23 09:37:00 -0700

    added if statement

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var app = express();
 
@@ -5,8 +6,13 @@ app.use("/public", express.static(__dirname + '/public'));
 
 // process.env.MESSAGE_STYLE='uppercase';
 
-app.get("/JSON", function(req, res) {
-    var response=({"message": "HELLO JSON"});
+app.get("/json", function(req, res) {
+    response.message=({"message": "Hello json"});
+
+    if(process.env.MESSAGE_STYLE='uppercase'){
+        response.message=response.message.toUpperCase();
+    }
+    return res.json(response);
 });
 
 
