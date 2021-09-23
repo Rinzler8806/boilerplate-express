6452751f7b5a5e4547d12810fe522e46d21fff77
Rinzler8806 <shane.rogers916@gmail.com>
2021-09-23 11:57:28 -0700

    testing

--- myApp.js
+++ myApp.js
@@ -9,13 +9,13 @@ app.use("/public", express.static(__dirname + '/public'));
 app.get("/json", function(req, res) {
 
     var response = "Hello json"
-    if (process.env.MESSAGE_STYLE == "uppercase") {
-      response = response.toUpperCase();
-    } 
-    res.json({
-      message: response
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({"message":"HELLO JSON"});
+      }
+      else{
+        res.json({"message":"Hello json"});
+      } 
     });
-  });
 
 
 
