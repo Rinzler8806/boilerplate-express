bb0c0f7aa4d7fea49e96ba436d7c7dc6f9228c34
Rinzler8806 <shane.rogers916@gmail.com>
2021-09-23 12:21:37 -0700

    testing

--- myApp.js
+++ myApp.js
@@ -1,23 +1,24 @@
 const { response } = require('express');
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 app.use("/public", express.static(__dirname + '/public'));
 
 // process.env.MESSAGE_STYLE='uppercase';
 
-app.get("/json", function (req, res) {
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-      res.json({
-        "message": "HELLO JSON"
-      });
-    }
-      res.json({
-        "message": "Hello json"
-      });  
-  });
+app.get("/json", function(req, res) {
 
+    var response = "Hello json"
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({"message":"HELLO JSON"});
+      }
+      else{
+        res.json({"message":"Hello json"});
+      } 
+    });
 
+    console.log(process.env.MESSAGE_STYLE)
 
 
 
