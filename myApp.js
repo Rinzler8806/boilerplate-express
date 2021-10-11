13fc54399193aef5883b7ff4f61b449ca973d06b
Rinzler8806 <shane.rogers916@gmail.com>
2021-10-10 21:35:28 -0700

    restarting with Hello World

--- myApp.js
+++ myApp.js
@@ -1,26 +1,29 @@
-const { response } = require('express');
+// const { response } = require('express');
 var express = require('express');
 var app = express();
-require('dotenv').config();
+// require('dotenv').config();
 
-app.use("/public", express.static(__dirname + '/public'));
+// app.use("/public", express.static(__dirname + '/public'));
 
-// process.env.MESSAGE_STYLE='uppercase';
+// // process.env.MESSAGE_STYLE='uppercase';
 
-app.get("/json", function(req, res) {
+// app.get("/json", function(req, res) {
 
-    var response = "Hello json"
-    if(process.env.MESSAGE_STYLE === 'uppercase'){
-        res.json({"message":"HELLO JSON"});
-      }
-      else{
-        res.json({"message":"Hello json"});
-      } 
-    });
+//     var response = "Hello json"
+//     if(process.env.MESSAGE_STYLE === 'uppercase'){
+//         res.json({"message":"HELLO JSON"});
+//       }
+//       else{
+//         res.json({"message":"Hello json"});
+//       } 
+//     });
 
-    console.log(process.env.MESSAGE_STYLE)
+//     console.log(process.env.MESSAGE_STYLE)
 
-
+// app.get("/json", function(req, res) {
+//   var response = "Hello World";
+  console.log("Hello World");
+// });
 
 
 
