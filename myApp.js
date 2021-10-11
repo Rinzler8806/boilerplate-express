a2bac20a904c548f43cd842d54aa5b3d3fabe7bf
Rinzler8806 <shane.rogers916@gmail.com>
2021-10-10 22:18:07 -0700

    update to fetch json

--- myApp.js
+++ myApp.js
@@ -7,16 +7,16 @@ app.use("/public", express.static(__dirname + '/public'));
 
 // // process.env.MESSAGE_STYLE='uppercase';
 
-// app.get("/json", function(req, res) {
+app.get("/json", function(req, res) {
 
-//     var response = "Hello json"
-//     if(process.env.MESSAGE_STYLE === 'uppercase'){
-//         res.json({"message":"HELLO JSON"});
-//       }
-//       else{
-//         res.json({"message":"Hello json"});
-//       } 
-//     });
+    var response = "Hello json"
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({"message":"HELLO JSON"});
+      }
+      else{
+        res.json({"message":"Hello json"});
+      } 
+    });
 
 //     console.log(process.env.MESSAGE_STYLE)
 
